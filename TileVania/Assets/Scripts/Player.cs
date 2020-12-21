c8ba9cd42ad7eb0fb4a97f4b3d6e4da0950ac6fa
Wintyswilly <Wintyboss@outlook.com>
2020-12-21 19:02:57 +0100

    Added hazards tile

--- TileVania/Assets/Scripts/Player.cs
+++ TileVania/Assets/Scripts/Player.cs
@@ -79,7 +79,7 @@ public class Player : MonoBehaviour
 
     private void Die() 
     {
-        if (myBodyColider.IsTouchingLayers(LayerMask.GetMask("Enemy")))
+        if (myBodyColider.IsTouchingLayers(LayerMask.GetMask("Enemy","Hazards")))
         {
             isAlive = false;
             myAnimator.SetTrigger("Dying");
